728e7a9cffc5f75cb67be060c996e4ebe4be01f6
Dhanushka Bandara <dhanushka_b@3logixs.lk>
2018-11-14 11:59:34 +0530

    updated database error

--- database/seeds/CitiesTableSeeder.php
+++ database/seeds/CitiesTableSeeder.php
@@ -11,11 +11,12 @@ class CitiesTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('provinces')->insert([
-            [‘city_district_id’=>1,
-            ‘city_name_en’=> 'Akkaraipattu',
-            ‘city_name_si’=>  'අක්කරපත්තුව', 
-            ‘city_name_ta’=>  '',
+        DB::table('cities')->insert([
+            ['city_district_id'=>1,
+            'city_province_id'=>6,
+            'city_name_en'=> 'Akkaraipattu',
+            'city_name_si'=>  'අක්කරපත්තුව', 
+            'city_name_ta'=>  '',
             'city_postcode'=>  '',
             'city_latitide'=>  '',
             'city_longitude'=>  '']
